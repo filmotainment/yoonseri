0e2de54eb6ea77634e0ffcfca0e2aefe52085279
Filmotainment <89825644+filmotainment@users.noreply.github.com>
2023-05-26 15:13:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/filmotainment/yoonseri.git /yoonseri
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /yoonseri
 fi
-cd /PiroAutoFilterBot
+cd /yoonseri
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting YoonSeriBot...."
 python3 bot.py
